240df2088505bf30be3390f536d88dc23ef65470
Fredrik Teschke <f@ftes.de>
2023-02-18 08:39:57 +0100

    Add bowling tests

--- bowling.exs
+++ bowling.exs
@@ -6,22 +6,32 @@ end
 
 defmodule Player do
   def chunk(rolls) do
-    Enum.chunk_while(rolls, {[], 1}, fn
-      r3, {[r1, r2], 10} -> {:halt, {[r1, r2, r3], 10}}
-      r2, {[r1], 10} when r1 + r2 < 10 -> {:halt, {[r1, r2], 10}}
-      r2, {[r1], 10} -> {:cont, {[r1, r2], 10}}
-      10, {[], frame} -> {:cont, [10], {[], frame + 1}}
-      r2, {[r1], frame} -> {:cont, [r1, r2], {[], frame + 1}}
-      r1, {[], frame} -> {:cont, {[r1], frame}}
-    end, fn
-      {frame, _} -> {:cont, frame, []}
-    end)
+    Enum.chunk_while(
+      rolls,
+      {[], 1},
+      fn
+        _, {_, 11} -> raise("unexpected")
+        r3, {[r2, r1], 10} -> {:halt, {[r1, r2, r3], 10}}
+        r2, {[r1], 10} when r1 + r2 < 10 -> {:halt, {[r1, r2], 10}}
+        r2, {r, 10} -> {:cont, {[r2 | r], 10}}
+        10, {[], frame} -> {:cont, [10], {[], frame + 1}}
+        r2, {[r1], frame} -> {:cont, [r1, r2], {[], frame + 1}}
+        r1, {[], frame} -> {:cont, {[r1], frame}}
+      end,
+      fn
+        {frame, _} -> {:cont, frame, []}
+      end
+    )
   end
 
-  def score(frames) do
+  def score([i | _] = rolls) when is_integer(i), do: rolls |> chunk() |> score()
+
+  def score([l | _] = frames) when is_list(l) do
     frames
     |> Enum.with_index()
-    |> Enum.map(fn {f, i} -> Frame.score(f, frames |> Enum.slice(i+1..-1) |> List.flatten()) end)
+    |> Enum.map(fn {f, i} ->
+      Frame.score(f, frames |> Enum.slice((i + 1)..-1) |> List.flatten())
+    end)
     # https://github.com/elixir-lang/elixir/issues/12416
     # |> Enum.chunk_every(3, 1, [[], []])
     # |> Enum.map(fn [f1, f2, f3] -> Frame.score(f1, f2 ++ f3) |> IO.inspect() end)
@@ -29,18 +39,19 @@ defmodule Player do
   end
 end
 
-[
-  1, 4,
-  4, 5,
-  6, 4,
-  5, 5,
-  10,
-  0, 1,
-  7, 3,
-  6, 4,
-  0, 10,
-  2, 8, 6
-]
-|> Player.chunk()
-|> Player.score()
-|> IO.inspect()
+ExUnit.start()
+
+defmodule Tests do
+  use ExUnit.Case, async: true
+
+  test "player score" do
+    assert 1 == [1, 0] |> Player.score()
+    assert 10 == [1, 9] |> Player.score()
+    assert 20 == [1, 9, 5, 0] |> Player.score()
+    assert 28 == [10, 5, 4] |> Player.score()
+    assert 48 == [10, 10, 4, 3] |> Player.score()
+    assert 115 == [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 0, 10, 2, 8, 6] |> Player.score()
+    assert 4 == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3] |> Player.score()
+    assert 30 == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10] |> Player.score()
+  end
+end
--- bowling.kts
+++ bowling.kts
@@ -2,8 +2,8 @@ interface Frame {
     val rolls: List<Int>
     var next: Frame?
     fun score(): Int = rolls.sum()
-    fun roll(): Int = rolls[0]
-    fun twoRolls(): List<Int> = rolls.slice(0..1)
+    fun firstRoll(): Int = rolls[0]
+    fun firstTwoRolls(): List<Int> = rolls.slice(0..1)
 }
 
 data class NormalFrame(override val rolls: List<Int>) : Frame {
@@ -14,18 +14,18 @@ data class SpareFrame(override val rolls: List<Int>) : Frame {
     override var next: Frame? = null
 
     override fun score(): Int {
-        return rolls.sum() + (next?.roll() ?: 0)
+        return rolls.sum() + (next?.firstRoll() ?: 0)
     }
 }
 
 data class StrikeFrame(override val rolls: List<Int> = listOf(10)) : Frame {
     override var next: Frame? = null
     override fun score(): Int {
-        return rolls.sum() + (next?.twoRolls()?.sum() ?: 0)
+        return rolls.sum() + (next?.firstTwoRolls()?.sum() ?: 0)
     }
 
-    override fun twoRolls(): List<Int> {
-        return listOf(rolls[0], next?.roll() ?: 0)
+    override fun firstTwoRolls(): List<Int> {
+        return listOf(rolls[0], next?.firstRoll() ?: 0)
     }
 }
 
@@ -43,7 +43,7 @@ class Player {
     }
 
     fun addRoll(roll: Int) {
-        if (isFinished) throw Exception("Finished")
+        require(!isFinished) { "is finished" }
 
         rolls += roll
         val sum = rolls.sum()
@@ -71,6 +71,18 @@ class Player {
     }
 }
 
-val player = Player()
-listOf(/*1*/ 1, 4, /*2*/ 4, 5, /*3*/ 6, 4, /*4*/ 5, 5, /*5*/ 0, 10, /*6*/ 0, 1, /*7*/ 7, 3, /*8*/ 6, 4, /*9*/ 0, 10, /*10*/ 2, 8, 6).forEach(player::addRoll)
-println(player.score())
+fun assertScore(expected: Int, rolls: List<Int>) {
+    val player = Player()
+    rolls.forEach(player::addRoll)
+    val actual = player.score()
+    if (expected != actual) throw AssertionError("score $expected != $actual")
+}
+
+assertScore(1, listOf(1, 0))
+assertScore(10, listOf(1, 9))
+assertScore(20, listOf(1, 9, 5, 0))
+assertScore(28, listOf(10, 5, 4))
+assertScore(48, listOf(10, 10, 4, 3))
+assertScore(115, listOf(/*1*/ 1, 4, /*2*/ 4, 5, /*3*/ 6, 4, /*4*/ 5, 5, /*5*/ 10, /*6*/ 0, 1, /*7*/ 7, 3, /*8*/ 6, 4, /*9*/ 0, 10, /*10*/ 2, 8, 6))
+assertScore(4, listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3))
+assertScore(30, listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10))
